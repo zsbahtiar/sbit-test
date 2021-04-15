1564aaf2d9249140001a4d1197623d169185fb9d
Bahtiar <zam@kitalulus.com>
2021-04-16 05:37:10 +0700

    add: scripst heroku prebuild and add try catch

--- 2_express/src/controller/index.js
+++ 2_express/src/controller/index.js
@@ -1,29 +1,55 @@
-import axios from 'axios';
-import config from '../../config';
-import helperLogs from '../helper';
-import logModel from '../model/logModel';
+import axios from "axios";
+import config from "../../config";
+import helperLogs from "../helper";
+import logModel from "../model/logModel";
 
 
-export async function searchController (req, res) {
-  const { query: {q= '' } } = req || {};
-  if(q === '') return res.send('Please use like: search?q={keyword}')
-  const response = await axios.get(`${config.ENDPOINT}&s=${q}`);
-  helperLogs('/search', q);
+export async function searchController(req, res) {
+  const {
+    query: { q = "" },
+  } = req || {};
+  if (q === "")
+    return res.send({ message: "Please use like: search?q={keyword}" });
 
-  return res.send(response?.data?.Search??{});
+  try {
+    const response = await axios.get(`${config.ENDPOINT}&s=${q}`);
+    helperLogs("/search", q);
+
+    return res.send(response?.data?.Search ?? {});
+  } catch (err) {
+    return res.send({
+      status: "failed",
+      err,
+    });
+  }
 }
 
-export async function detailController (req, res){
-  const { params: { id } } = req || {};
-  const response = await axios.get(`${config.ENDPOINT}&i=${id}`);
-  helperLogs('/detail/', id);
+export async function detailController(req, res) {
+  const {
+    params: { id },
+  } = req || {};
+  try {
+    const response = await axios.get(`${config.ENDPOINT}&i=${id}`);
+    helperLogs("/detail/", id);
 
-  return res.send(response?.data?? {});
+    return res.send(response?.data ?? {});
+  } catch (err) {
+    return res.send({
+      status: "failed",
+      err,
+    });
+  }
 }
 
-export async function logController(req,res) {
-  const response = await logModel().findAll();
-  helperLogs('/log');
-  
-  return res.send(response);
+export async function logController(req, res) {
+  try {
+    const response = await logModel().findAll();
+    helperLogs("/log");
+    return res.send(response);
+  } catch (err) {
+    return res.send({
+      status: "failed",
+      err,
+    });
+  }
 }
