1a2b7816d62942e04cb9a7dbd758d9ec66b5f308
Bahtiar <zam@kitalulus.com>
2021-04-16 00:23:24 +0700

    add: endpoint log

--- 2_express/src/index.js
+++ 2_express/src/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import {
+  searchController,
+  detailController,
+  logController,
+} from './controller';
+import list from '../list.json';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-  res.send('Hello, world');
+  res.send(list);
 })
+app.get('/search', searchController);
+app.get('/detail/:id', detailController);
+app.get('/log', logController);
+
 
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log(`Express is running`);
 })
