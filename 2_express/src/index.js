3683dd1b48eca06dc7519d9084113120244121dd
Bahtiar <zam@kitalulus.com>
2021-04-15 14:04:43 +0700

    initital commit

---
+++ 2_express/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+})
+
+
+app.listen(3000, () => {
+  console.log(`Express is running`);
+})
