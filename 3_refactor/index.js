45f149f0e4d9f8153be6ad8e2bf45e0b7e840e24
Bahtiar <zam@kitalulus.com>
2021-04-15 10:08:07 +0700

    add: solution for refactor

---
+++ 3_refactor/index.js
@@ -0,0 +1,61 @@
+/* Refactor from this 
+
+function findFirstStringInBracket(str) {
+  if (str.length > 0) {
+    let indexFirstBracketFound = str.indexOf("(");
+    if (indexFirstBracketFound >= 0) {
+      let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
+      if (wordsAfterFirstBracket) {
+        wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
+        let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+        if (indexClosingBracketFound >= 0) {
+          return wordsAfterFirstBracket.substring(0, indexClosingBracketFound);
+        } else {
+          return "";
+        }
+      } else {
+        return "";
+      }
+    } else {
+      return "";
+    }
+  } else {
+    return "";
+  }
+}
+
+
+/* to this */
+
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ */
+const findFirstStringInBracket = (str = "") => {
+  if (str.length > 0) {
+    const searchData = {
+      indexOfFirstBracket: null,
+      indexOfCloseFirstBracket: null,
+    };
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "(" && searchData.indexOfFirstBracket === null) {
+        searchData.indexOfFirstBracket = i;
+      } else if (str[i] === ")" && searchData.indexOfCloseFirstBracket === null) {
+        searchData.indexOfCloseFirstBracket = i;
+        break;
+      }
+    }
+    return searchData.indexOfCloseFirstBracket === null ||
+      searchData.indexOfFirstBracket === null
+      ? ""
+      : str.substring(
+          searchData.indexOfFirstBracket + 1,
+          searchData.indexOfCloseFirstBracket
+        );
+  }
+
+  return "";
+};
+
+console.log(findFirstStringInBracket("Negara (INDONESIA) itu cantik!"));
